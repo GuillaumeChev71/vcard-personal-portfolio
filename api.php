760d2a0630daa8550ac4c211cb6e6394c8c49631
Guillaume Chevallier <guillaumechevallier71@gmail.com>
2025-01-09 23:59:48 +0100

    toto

--- api.php
+++ api.php
@@ -1,30 +1,31 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   $data = json_decode(file_get_contents("php://input"), true);
+    $data = json_decode(file_get_contents("php://input"), true);
 
-   if (isset($data['action']) && $data['action'] === 'maFonctionPHP') {
-      // Récupérez les paramètres
-      $nomSender = $data['nomSender'];
-      $mailSender = $data['mailSender'];
-      $messageSender = $data['messageSender']; 
+    if (isset($data['action']) && $data['action'] === 'maFonctionPHP') {
+        // Récupérez les paramètres
+        $nomSender = $data['nomSender'];
+        $mailSender = $data['mailSender'];
+        $messageSender = $data['messageSender'];
 
-      // Appelez votre fonction avec les paramètres
-      maFonctionPHP($nomSender,$mailSender,$messageSender);
-  }
+        // Appelez votre fonction avec les paramètres
+        maFonctionPHP($nomSender, $mailSender, $messageSender);
+    }
 }
 
-function maFonctionPHP($nomSender,$mailSender,$messageSender) {
-   // Code de votre fonction PHP
-   $to = 'dev@itgcdev.fr';
+function maFonctionPHP($nomSender, $mailSender, $messageSender)
+{
+    // Code de votre fonction PHP
+    $to = 'dev@itgcdev.fr';
     $subject = 'Prise de contact';
-    $message = "Nom : "."$nomSender\n".
-    "Mail : "."$mailSender\n".
-    "Message : "."$messageSender\n";
+    $message = "Nom : " . "$nomSender\n" .
+        "Mail : " . "$mailSender\n" .
+        "Message : " . "$messageSender\n";
 
     // Vous pouvez ajouter des en-têtes supplémentaires si nécessaire
     $headers = 'From: contact@example.com' . "\r\n" .
-               'Reply-To: expediteur@example.com' . "\r\n" .
-               'X-Mailer: PHP/' . phpversion();
+        'Reply-To: expediteur@example.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 
     // Essayez d'envoyer l'e-mail
     $success = mail($to, $subject, $message, $headers);
@@ -37,4 +38,3 @@ function maFonctionPHP($nomSender,$mailSender,$messageSender) {
         return false;
     }
 }
-?>
